1fd0b1e16625be702970ab4127ecc27a63e7711b
George Downing <george.downing20@icloud.com>
2022-10-13 19:33:22 +0100

    add main.h

--- main.c
+++ main.c
@@ -1,38 +1,19 @@
 //-------------------------------------------------------------
-//path: main.c
-//author: G. Downing
-//created on: 10-10-2022
-//last modified: 10-10-2022
-//version: 1.0
-//language: c
-//Copyright (c) 2022 G. Downing
-//Description: function to estimate to value of pi using the monte carlo method
+//@path: main.c
+//@author: G. Downing
+//@created on: 10-10-2022
+//@last modified: 10-10-2022
+//@version: 1.0
+//@language: c
+//@Copyright (c) 2022 G. Downing
+//@Description: function to estimate to value of pi using the monte carlo method
 //-------------------------------------------------------------
 
-#include <stdio.h>  // printf and friends
-#include <stdlib.h> // access to rand functions
+#include "main.h"
 
-#ifndef STATUS_E // if STATUS_E is not defined
-#define STATUS_E // define it
-enum status      // create an enum called status
-{
-    SUCCESS, // set SUCCESS to 0
-    FAILURE, // set FAILURE to 1
-    ERROR    // set ERROR to 2
-};
-#endif // end if
-
-// function: get_monte_carlo_pi
-// purpose: estimate pi using monte carlo method (https://en.wikipedia.org/wiki/Monte_Carlo_method)
-// input: number of iterations and pointer to pi
-// output: estimated value of pi
-// return: status of function (SUCCESS or ERROR)
-// author: G. Downing
-// date: 10-10-2022
-// version: 1.0
-// language: c
-// requires: stdio.h, stdlib.h and status enum
-enum status get_monte_carlo_pi(int n, double *pi); // function prototype
+//-------------------------------------------------------------
+//                  function definitions
+//-------------------------------------------------------------
 
 enum status get_monte_carlo_pi(int n, double *pi) // task B
 {
@@ -59,16 +40,20 @@ enum status get_monte_carlo_pi(int n, double *pi) // task B
     return SUCCESS;              // return SUCCESS code to be handled in main
 }
 
-// function: main
-// purpose: tests get_monte_carlo_pi function
-// input: gets number of iterations from command line
-// output: prints estimated value of pi
-// return: 0
-// author: G. Downing
-// date: 10-10-2022
-// version: 1.0
-// language: c
-// requires: stdio.h, stdlib.h and status enum
+//-------------------------------------------------------------
+//                  main functions
+//-------------------------------------------------------------
+
+// @function: main
+// @purpose: tests get_monte_carlo_pi function
+// @input: gets number of iterations from command line
+// @output: prints estimated value of pi
+// @return: 0
+// @author: G. Downing
+// @date: 10-10-2022
+// @version: 1.0
+// @language: c
+// @requires: stdio.h, stdlib.h and status enum
 int main(void)
 {
     // initialize local variables
---
+++ main.h
@@ -0,0 +1,44 @@
+//-------------------------------------------------------------
+//@path: main.c
+//@author: G. Downing
+//@created on: 10-10-2022
+//@last modified: 10-10-2022
+//@version: 1.0
+//@language: c
+//@Copyright (c) 2022 G. Downing
+//@Description: Header file for function to estimate to value of pi using the monte carlo method
+//-------------------------------------------------------------
+
+//-------------------------------------------------------------
+//              Preprocessor directives
+//-------------------------------------------------------------
+
+#include <stdio.h>  // printf and friends
+#include <stdlib.h> // access to rand functions
+
+//-------------------------------------------------------------
+//              data types and structures
+//-------------------------------------------------------------
+
+enum status // create an enum called status
+{
+    SUCCESS, // set SUCCESS to 0
+    FAILURE, // set FAILURE to 1
+    ERROR    // set ERROR to 2
+};
+
+//-------------------------------------------------------------
+//              Function prototypes
+//-------------------------------------------------------------
+
+// @function: get_monte_carlo_pi
+// @purpose: estimate pi using monte carlo method (https://en.wikipedia.org/wiki/Monte_Carlo_method)
+// @input: number of iterations and pointer to pi
+// @output: estimated value of pi
+// @return: status of function (SUCCESS or ERROR)
+// @author: G. Downing
+// @date: 10-10-2022
+// @version: 1.0
+// @language: c
+// @requires: stdio.h, stdlib.h and status enum
+enum status get_monte_carlo_pi(int n, double *pi); // function prototype
